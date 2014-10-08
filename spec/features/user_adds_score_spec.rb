e4910ab15e8449e263feef34c8ce59b2898efe23
David Sok <sokd88@gmail.com>
2014-10-08 13:36:10 -0400

    rspec pass for user not inputting required field: score and date

--- spec/features/user_adds_score_spec.rb
+++ spec/features/user_adds_score_spec.rb
@@ -14,8 +14,22 @@ feature 'user adds a score' do
     visit golf_course_path(course)
     fill_in "Score", with: score.score
     fill_in "Date", with: score.date
-    save_and_open_page
     click_on "Add Score"
     expect(page).to have_content("Score was successfully submitted!")
   end
+    scenario  'user doesnt input all neccessary field' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    course = FactoryGirl.create(:golf_course)
+    score = FactoryGirl.build(:score)
+    visit golf_course_path(course)
+    click_on "Add Score"
+    expect(page).to have_content("Score can't be blank")
+    expect(page).to have_content("Date can't be blank")
+  end
 end
+

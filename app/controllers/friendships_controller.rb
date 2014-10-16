b41422c49262dfbe21dbc95e282d681679b127f6
David Sok <sokd88@gmail.com>
2014-10-15 21:05:29 -0400

    added background for submit score and profile

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -15,7 +15,7 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    @friendship = current_user.friendships.find(params[:id])
+    @friendship = current_user.friendships.find_by(user_id: params[:user_id])
     @friendship.destroy
     flash[:notice] = "Removed Friend"
     redirect_to current_user
